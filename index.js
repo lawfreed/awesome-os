e809405ac6cb543163e816249d99b03e47775f54
Mark Lawfreed <lawfreed@protonmail.com>
2018-09-14 08:51:57 +0800

    Reconfigure assets directory XP005

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+app.use('*/images',express.static('assets/images'));
+app.use('*/videos',express.static('assets/media/images'));
 app.listen(process.env.PORT || 8080, () => console.log('All is okay!'));
