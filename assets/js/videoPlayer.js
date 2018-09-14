dae1b46eb3e5224cc394c5740d1ecaf3b128ac78
Mark Lawfreed <lawfreed@protonmail.com>
2018-09-14 10:05:40 +0800

    Revert 'videoPlayer.js'

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,5 +1,4 @@
 const video = document.getElementById("videoPlayer");
-const playPromise = video.play();
 let flip = true,
   pause = "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28",
   play = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26",
@@ -12,11 +11,8 @@ export const videoPlayerCommand = () => {
     "to": flip ? play : pause
   }).get(0).beginElement();
   if (video.paused) {
-    playPromise.then(_ => {
-      // Automatic playback started!
-      // Show playing UI.
-      // We can now safely pause video...
-      video.pause();
-    })
+    video.play();
+  } else {
+    video.pause();
   }
 }
--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -116,7 +116,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var video = document.getElementById("videoPlayer");
-var playPromise = video.play();
 var flip = true,
     pause = "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28",
     play = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26",
@@ -129,12 +128,9 @@ var videoPlayerCommand = exports.videoPlayerCommand = function videoPlayerComman
     "to": flip ? play : pause
   }).get(0).beginElement();
   if (video.paused) {
-    playPromise.then(function (_) {
-      // Automatic playback started!
-      // Show playing UI.
-      // We can now safely pause video...
-      video.pause();
-    });
+    video.play();
+  } else {
+    video.pause();
   }
 };
 
