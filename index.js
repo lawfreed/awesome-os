8f091bb7126a503430215c177c73e9673abbfac2
Mark Lawfreed <lawfreed@protonmail.com>
2018-09-14 08:59:42 +0800

    Reconfigure fonts assets directory XP007.A

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express();
 app.use(express.static('public'));
 app.use('*/images',express.static('assets/images'));
 app.use('*/videos',express.static('assets/media/videos'));
+app.use('*/fonts',express.static('assets/fonts'));
 app.listen(process.env.PORT || 8080, () => console.log('All is okay!'));
